52127f6becceb7281ae2633a1b6b1310dc57e4e0
Shivam Chaudhary <shivam9940@gmail.com>
2024-05-25 18:39:45 +0530

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple ToDo App built in react js
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+import ToDoList from './components/ToDoList.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ToDoList />
+    </>
+  )
+}
+
+export default App
